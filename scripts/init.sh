f430e04cf97da238f2a6daea3f1ac5037612977f
BrunoChauvet <bruno.chauvet@live.fr>
2015-06-25 21:13:10 +1000

    Trap signals

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,9 +1,28 @@
 #!/bin/bash
 
-# Execute configuration script
-/root/configure.py
+function start_all() {
+  # Execute configuration script
+  /root/configure.py
 
-# Start services
-/etc/init.d/mysql start
-/etc/init.d/apache2 start
-/bin/bash
+  # Start services
+  /etc/init.d/mysql start
+  /etc/init.d/apache2 start
+  /bin/bash
+}
+
+function reload_all() {
+  echo "Reloading..."
+  /etc/init.d/mysql restart
+  /etc/init.d/apache2 restart
+}
+
+function stop_all() {
+  echo "Shutting down..."
+  /etc/init.d/mysql stop
+  /etc/init.d/apache2 stop
+}
+
+trap stop_all EXIT
+trap reload_all SIGHUP
+
+start_all
